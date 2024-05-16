94330b7ec30eb8ac712ce9ecc578e05d6f247c66
kyunghwa <snoopn303@gmail.com>
2024-05-16 21:18:50 +0900

    붓의 두께 조절하기(화면에 맞춰)

--- app/src/main/java/com/wack/drawing/DrawingView.kt
+++ app/src/main/java/com/wack/drawing/DrawingView.kt
@@ -3,6 +3,7 @@ package com.wack.drawing
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintSet.Motion
@@ -34,7 +35,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         // 이미지나 그래픽의 부드럽고 자연스러운 외관을 유지하는 기술
         // 색상밴드 : 이미지나 그래픽에서 발생하는 색상의 부드럽지 못한 변화를 의미
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+//        mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -93,6 +94,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         // invalidate :
         invalidate()
         return true
+    }
+
+    // Brush 두께 조절
+    fun setSizeForBrush(newSize : Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
--- app/src/main/java/com/wack/drawing/MainActivity.kt
+++ app/src/main/java/com/wack/drawing/MainActivity.kt
@@ -2,10 +2,20 @@ package com.wack.drawing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wack.drawing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var drawingView: DrawingView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Brush의 두께 조절
+        binding.drawingView.setSizeForBrush(20.toFloat())
     }
 }
