ffb22c2dd188b66846ad5221c7729a7165125d1a
kyunghwa <snoopn303@gmail.com>
2024-07-09 22:34:29 +0900

    minSdk 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.wack.drawing"
-        minSdk = 26
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
